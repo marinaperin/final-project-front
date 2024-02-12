9451054e5f1d455f201661b2998de96c2b7f6f61
Marina Perin <maryfrancisco@live.com>
2024-02-12 19:36:11 +0100

    feat | about page with style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,13 @@
-export default function(){
-    
+export default function () {
+  return (
+    <main className="about-page">
+      <h1 className="about-h1">About</h1>
+      <div>
+        Personal project created as final project for the school I attended,
+        using the MERN stack: React, Node.js with Express and MongoDB. Directly
+        connected with a self-made API.
+      </div>
+      <img src="../../public/ghost.png" alt="" className="ghost"/>
+    </main>
+  );
 }
