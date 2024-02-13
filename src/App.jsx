489188cdb0c5f65a379a30603e9d9d88a4060155
Marina Perin <maryfrancisco@live.com>
2024-02-13 12:14:37 +0100

    feat | creatures page with style

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "./components/About";
 import NotFound from "./components/NotFound";
 import SignUp from "./components/SignUp-LogIn/SignUp";
 import LogIn from "./components/SignUp-LogIn/LogIn";
+import Creatures from "./components/Creatures/Creatures";
 
 function App() {
   return (
@@ -14,11 +15,21 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-       {/*  <Route path="/creatures" index></Route>
-        <Route path="/cultures" index></Route> */}
-        <Route path="/sign-up" element={<SignUp/>}/> 
-        <Route path="/log-in" element={<LogIn/>}/>
-       {/*  <Route path="/favorites"/> */}
+        <Route path="/creatures">
+          <Route index element={<Creatures />} />
+          <Route path=":id" element />
+        </Route>
+        <Route path="/cultures">
+          <Route index element />
+          <Route path=":id" element />
+          <Route path="/cultures/events">
+            <Route index element />
+            <Route path=":id" element/>
+          </Route>
+        </Route>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/log-in" element={<LogIn />} />
+        <Route path="/favorites" element />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
---
+++ src/components/Creatures/Creatures.jsx
@@ -0,0 +1,94 @@
+import axios from "axios";
+import "./creatures.scss";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+const { VITE_API_URL } = import.meta.env;
+
+export default function () {
+  const [data, setData] = useState({});
+  const [creatures, setCreatures] = useState([]);
+  const [error, setError] = useState(false);
+  const [n, setN] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`${VITE_API_URL}/creatures?page=${n}`)
+      .then((res) => {
+        console.log(res);
+        setData({ ...res.data });
+        setCreatures(res.data.results);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(true);
+      });
+  }, [n]);
+
+  return (
+    <main className="creatures-page">
+      {error && "There was an error, try again in a few minutes."}
+      {!error && !data && "Loading..."}
+      {!error && data && (
+        <>
+          <header>
+            <img
+              src="../public/skeleton.png"
+              alt=""
+              className="skeleton tablet-hide"
+            />
+            <h1>Creatures | Entities | Deities</h1>
+            <div className="creatures-info">
+              <p>Total results: {data.total_results}</p>
+              <p>Total pages: {data.total_pages}</p>
+            </div>
+            <div className="pages-btn">
+              <button
+                onClick={() => {
+                  setN(1);
+                }}
+              >
+                1
+              </button>
+              <button
+                onClick={() => {
+                  setN(2);
+                }}
+              >
+                2
+              </button>
+              <button
+                onClick={() => {
+                  setN(3);
+                }}
+              >
+                3
+              </button>
+            </div>
+          </header>
+          <section className="creatures-grid">
+            {creatures.map((c, i) => {
+              return (
+                <div key={`${c.name} + ${i}`} className="creature-card">
+                  <Link to={`creature/${c._id}`}>
+                    <div>
+                      <h3>{c.name}</h3>
+                      <div className="intro-card">
+                        <p>{c.type[0]}</p>
+                        <p>{c.culture.country}</p>
+                      </div>
+                    </div>
+                    <div className="lower-card">
+                      <figure>
+                        <img src={c.img} alt={c.name} />
+                      </figure>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+          </section>
+        </>
+      )}
+    </main>
+  );
+}
